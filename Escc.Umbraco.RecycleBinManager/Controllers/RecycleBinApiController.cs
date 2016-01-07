4312467c605a57209c01863d0f778228c908686a
Gordon Saxby <gordon.saxby@eastsussex.gov.uk>
2016-01-07 15:52:34 +0000

    Initial commit

---
+++ Escc.Umbraco.RecycleBinManager/Controllers/RecycleBinApiController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace Escc.Umbraco.RecycleBinManager.Controllers
+{
+    [Authorize]
+    public class RecycleBinApiController : UmbracoApiController
+    {
+        private int _noOfDaysOlderThan;
+        // UmbracoApiController exposes:
+        // ApplicationContext ApplicationContext { get; }
+        // ServiceContext Services { get; }
+        // DatabaseContext DatabaseContext { get; }
+        // UmbracoHelper Umbraco { get; }
+        // UmbracoContext UmbracoContext { get; }
+
+        //private readonly IContentService _contentService;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="noOfDaysOlderThan">
+        /// 
+        /// </param>
+        /// <returns>
+        /// 
+        /// </returns>
+        [HttpGet]
+        [HttpPost]
+        public HttpResponseMessage CleanRecycleBins(int noOfDaysOlderThan)
+        {
+            try
+            {
+                _noOfDaysOlderThan = noOfDaysOlderThan;
+
+                // Check that the correct credentials have been supplied
+                //var content = Request.Content.ReadAsStringAsync().Result;
+                //if (!Authentication.AuthenticateUser(content))
+                //{
+                //    return Request.CreateResponse(HttpStatusCode.Forbidden);
+                //}
+
+                // OK, carry on
+                return DeleteOldRecycleBinContent();
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        /// <summary>
+        /// Delete items in Content and Media recycle bins that are older than the number of days
+        /// defined in web.config.
+        /// </summary>
+        /// <returns></returns>
+        private HttpResponseMessage DeleteOldRecycleBinContent()
+        {
+            var triggerDate = DateTime.Now.AddDays(_noOfDaysOlderThan * -1);
+
+            // Content section Recycle Bin
+            var contentNodes = Services.ContentService.GetContentInRecycleBin().Where(n => n.UpdateDate > triggerDate);
+
+            foreach (var node in contentNodes)
+            {
+                //Services.ContentService.Delete(node);
+            }
+
+            // Media section Recycle Bin
+            var mediaNodes = Services.MediaService.GetMediaInRecycleBin().Where(n => n.UpdateDate > triggerDate);
+
+            foreach (var node in mediaNodes)
+            {
+                //Services.MediaService.Delete(node);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Escc.Umbraco.RecycleBinManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Escc.Umbraco.RecycleBinManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Escc.Umbraco.RecycleBinManager")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9a8a0cd3-e121-4436-b9d5-4cedd3eff227")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
